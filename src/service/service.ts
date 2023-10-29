75da804f36735d6242377e9cdda1849e18d26488
shoxa <sllx.ok@gmail.com>
2023-10-29 22:58:24 +0500

    feat: add services

---
+++ src/service/service.ts
@@ -0,0 +1,21 @@
+import { IResponse } from '../types/types';
+
+export const fetchHeroes = async (): Promise<IResponse> => {
+  try {
+    const response = await fetch('https://swapi.dev/api/people/');
+    return await response.json();
+  } catch {
+    throw new Error('Unable to fetch posts');
+  }
+};
+
+export const searchHeroes = async (search: string): Promise<IResponse> => {
+  try {
+    const response = await fetch(
+      `https://swapi.dev/api/people/?search=${search}`,
+    );
+    return await response.json();
+  } catch {
+    throw new Error('Unable to fetch posts');
+  }
+};
