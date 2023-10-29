3356652869cd0279d7e60eab943e0e2c9af2a576
shoxa <sllx.ok@gmail.com>
2023-10-29 22:58:14 +0500

    feat: add types

---
+++ src/types/types.ts
@@ -0,0 +1,25 @@
+export interface IResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: IHero[];
+}
+
+export interface IHero {
+  name: string;
+  height: number;
+  mass: number;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: Date;
+  edited: Date;
+  url: string;
+}
