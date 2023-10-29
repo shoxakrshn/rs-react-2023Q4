d5b9ad7635c68fa75cce6b86b7c12b8ae74d1936
shoxa <sllx.ok@gmail.com>
2023-10-30 02:02:56 +0500

    refactor: change url in fetchHeroes

--- src/service/service.ts
+++ src/service/service.ts
@@ -2,7 +2,7 @@ import { IResponse } from '../types/types';
 
 export const fetchHeroes = async (): Promise<IResponse> => {
   try {
-    const response = await fetch('https://swapi.dev/api/people/');
+    const response = await fetch('https://swapi.dev/api/people/?page=1');
     return await response.json();
   } catch {
     throw new Error('Unable to fetch posts');
