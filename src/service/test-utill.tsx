ee73e6035a3be17af59cb9d20d63bf71879e5103
shoxa <sllx.ok@gmail.com>
2023-11-13 00:14:48 +0500

    feat: add vitest and RTL

---
+++ src/service/test-utill.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { ContextProvider } from '../context/ContextProvider';
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <MemoryRouter>
+      <ContextProvider>{children}</ContextProvider>
+    </MemoryRouter>
+  );
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
