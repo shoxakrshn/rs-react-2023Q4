c176c5644725587b294ab06ca64e7c24cf05de67
shoxa <sllx.ok@gmail.com>
2023-11-13 12:33:10 +0500

    feat: configure handler for msw

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,11 @@
-import { http } from 'msw';
-
+import { http, HttpResponse } from 'msw';
+import { mockResponse, mockDetail } from '../service/mockData';
 export const handlers = [
-  http.get('https://api.disneyapi.dev/character/', () => {}),
+  http.get('https://api.disneyapi.dev/character/', () => {
+    return HttpResponse.json(mockResponse, { status: 200 });
+  }),
+
+  http.get('https://api.disneyapi.dev/character/112', () =>
+    HttpResponse.json(mockDetail, { status: 200 }),
+  ),
 ];
