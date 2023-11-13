46d56645b76b6d030e6418a939011640096b5a83
shoxa <sllx.ok@gmail.com>
2023-11-13 18:36:04 +0500

    feat: add tests for Search

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -1,25 +1,29 @@
 import { render, screen, fireEvent } from '../../service/test-utill';
 import SearchBar from './SearchBar';
 
-describe('Search', () => {
-  it('shows typed text', () => {
+describe('Tests for the Search component', () => {
+  it('Verify that clicking the Search button saves the entered value to the local storage', () => {
     render(<SearchBar setItemsPerPage={() => {}} />);
     const input = screen.getByRole('textbox');
 
     fireEvent.change(input, { target: { value: 'fukuoka' } });
 
+    const submitButton = screen.getByRole('button', { name: 'Get heroes' });
+    fireEvent.click(submitButton);
+
+    const savedSearchValue = localStorage.getItem('searchKey');
+
     expect(screen.getByDisplayValue('fukuoka')).toBeInTheDocument();
+    expect(savedSearchValue).toEqual('fukuoka');
   });
 
-  // it('saves value in the localstorage', () => {
-  //   const { unmount } = render(<SearchBar setItemsPerPage={() => {}} />);
-  //   const input = screen.getByRole('textbox');
-  //   fireEvent.change(input, { target: { value: 'after change' } });
+  it('Check that the component retrieves the value from the local storage upon mounting', () => {
+    const savedValue = 'new fukuoka';
+    localStorage.setItem('searchKey', savedValue);
 
-  //   unmount();
+    render(<SearchBar setItemsPerPage={() => {}} />);
 
-  //   expect(localStorage.getItem('searchKey')).toBe(
-  //     (input as HTMLInputElement).value,
-  //   );
-  // });
+    const input = screen.getByRole('textbox');
+    expect(input).toHaveValue(savedValue);
+  });
 });
