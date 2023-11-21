deddf125427ca1e4ba601231adc40fc079026a5b
shoxa <sllx.ok@gmail.com>
2023-11-21 23:00:28 +0500

    refactor: change logic of Loading

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -2,27 +2,34 @@ import Item from '../Item/Item';
 import Pagination from '../Pagination/Pagination';
 import Loader from '../Loader/Loader';
 import { Outlet, useSearchParams } from 'react-router-dom';
-import { useAppSelector } from '../../redux/hooks';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { useGetCharactersQuery } from '../../redux/api';
 import { selectBasic } from '../../redux/slice';
+import { useEffect } from 'react';
+import { updateLoaderSearch } from '../../redux/slice';
 
 const List: React.FC = () => {
-  const { search, pageSize } = useAppSelector(selectBasic);
+  const { search, pageSize, loaderSearch } = useAppSelector(selectBasic);
+  const dispatch = useAppDispatch();
 
   const [searchParams] = useSearchParams();
   const currentPage = searchParams.get('page') as string;
 
-  const { data, isLoading } = useGetCharactersQuery({
+  const { data, isFetching } = useGetCharactersQuery({
     search,
     pageSize,
     pageNumber: +currentPage || 1,
   });
 
+  useEffect(() => {
+    dispatch(updateLoaderSearch(isFetching));
+  }, [isFetching]);
+
   if (data?.data.length === 0) {
     return <p>no results</p>;
   }
 
-  if (isLoading) return <Loader />;
+  if (loaderSearch) return <Loader />;
 
   return (
     <main>
--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -2,13 +2,22 @@ import { useParams } from 'react-router-dom';
 import Loader from '../../components/Loader/Loader';
 import { useGetDetailsQuery } from '../../redux/api';
 import DetailCard from '../../components/DeatilCard/DetailCard';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { selectBasic, updateLoaderDetails } from '../../redux/slice';
+import { useEffect } from 'react';
 
 const DetailPage: React.FC = () => {
   const { id } = useParams();
+  const { loaderDetails } = useAppSelector(selectBasic);
+  const dispatch = useAppDispatch();
 
-  const { data, isLoading } = useGetDetailsQuery(id as string);
+  const { data, isFetching } = useGetDetailsQuery(id as string);
 
-  if (isLoading) return <Loader />;
+  useEffect(() => {
+    dispatch(updateLoaderDetails(isFetching));
+  }, [isFetching]);
+
+  if (loaderDetails) return <Loader />;
 
   return <DetailCard character={data?.data} />;
 };
