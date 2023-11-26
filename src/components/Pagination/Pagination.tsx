a15ce59f8ecc0548c1687b95fc244e128fb58167
shoxa <sllx.ok@gmail.com>
2023-11-27 00:50:48 +0500

    refactor: refactor update current page

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -4,6 +4,7 @@ import {
   updateNextPage,
   updatePrevPage,
 } from '@/store/slices/page.slice';
+import { useRouter } from 'next/router';
 
 type PropsType = {
   nextPage: string | null;
@@ -11,14 +12,17 @@ type PropsType = {
 };
 
 const Pagination: React.FC<PropsType> = ({ nextPage, prevPage }) => {
+  const router = useRouter();
   const { currentPage } = useAppSelector(selectPage);
   const dispatch = useAppDispatch();
 
   const onPrevPage = () => {
     dispatch(updatePrevPage());
+    router.push(`/?page=${currentPage}`);
   };
   const onNextPage = () => {
     dispatch(updateNextPage());
+    router.push(`/?page=${currentPage}`);
   };
 
   return (
