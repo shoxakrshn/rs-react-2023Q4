642c11aeaf1986a74c35d6d9259c54511ba151e9
shoxa <sllx.ok@gmail.com>
2023-12-04 00:42:45 +0500

    feat: add routes from react-router-dom

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   Route,
   createBrowserRouter,
@@ -6,11 +5,15 @@ import {
 } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
+import UncontrolledPage from './pages/UncontrolledPage';
+import ControlledPage from './pages/ControlledPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<MainPage />} />
+      <Route index element={<MainPage />} />
+      <Route path="uncontrolled" element={<UncontrolledPage />} />
+      <Route path="controlled" element={<ControlledPage />} />
     </Route>,
   ),
 );
