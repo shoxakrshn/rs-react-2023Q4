ab16a9e10f2cf8775e53eab223f80f8d34f95dcb
shoxa <sllx.ok@gmail.com>
2023-12-04 01:57:14 +0500

    refactor: add basic UI to show saved data

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,32 +1,41 @@
-import { useState } from 'react';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useAppSelector } from '../store/hooks';
+import { selectControl } from '../store/slices/control.slice';
+import { Link } from 'react-router-dom';
+import { selectUncontrol } from '../store/slices/uncontrol.slice';
 
 const MainPage: React.FC = () => {
-  const [count, setCount] = useState(0);
+  const controlFormData = useAppSelector(selectControl);
+  const uncontrolFormData = useAppSelector(selectUncontrol);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav>
+        <Link to="controlled" className="mr-16">
+          Controlled
+        </Link>
+        <Link to="uncontrolled">Uncontrolled</Link>
+      </nav>
+      <div className="flex gap-8">
+        <ul>
+          controled Form:
+          <li>Name: {controlFormData.name}</li>
+          <li>Age: {controlFormData.age}</li>
+          <li>Email: {controlFormData.email}</li>
+          <li>Password: {controlFormData.password}</li>
+          <li>Country: {controlFormData.country}</li>
+          <li>
+            <img src={`data:image/png;base64,${controlFormData.picture}`} />
+          </li>
+        </ul>
+        <ul>
+          uncontroled Form:
+          <li>Name: {uncontrolFormData.name}</li>
+          <li>Age: {uncontrolFormData.age}</li>
+          <li>Email: {uncontrolFormData.email}</li>
+          <li>Password: {uncontrolFormData.password}</li>
+          <li>Country: {uncontrolFormData.country}</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
