8405a13a3fd71c9805c148d58cdd4cf3e14632b0
shoxa <sllx.ok@gmail.com>
2023-12-04 00:41:27 +0500

    feat: add types

---
+++ src/utils/types.ts
@@ -0,0 +1,11 @@
+export type FormDataType = {
+  name: string;
+  age: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  gender: string;
+  agreement: string;
+  picture: string;
+  country: string;
+};
