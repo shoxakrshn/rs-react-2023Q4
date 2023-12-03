1977e48b8eea2cf99fcab0034b5cb9d45a7775a5
shoxa <sllx.ok@gmail.com>
2023-12-04 01:56:27 +0500

    refactor: update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,11 @@
 export type FormDataType = {
   name: string;
-  age: string;
+  age: number;
   email: string;
   password: string;
   confirmPassword: string;
   gender: string;
-  agreement: string;
+  agreement: boolean;
   picture: string;
   country: string;
 };
